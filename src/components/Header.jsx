f89e9545be375eed005c1051ae8c27bb3a597d84
ivanlafa <ivanlafa98@gmail.com>
2023-05-16 10:32:53 -0500

    prueba

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ function Header() {
   return (
     <header className="grid">
       <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto">
-        Seguimiento pacientes {""} <span className="text-indigo-600">Veterinaria</span>
+        Seguimiento pacientes {""} Probando desde git {" "} <span className="text-indigo-600">Veterinaria</span>
       </h1>
     </header>
   );
